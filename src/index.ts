3eec9cf0165d36dbc2ffdf254ade557c1ecac76c
Shio3001 <taku2000maru@icloud.com>
2025-07-11 05:12:22 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { get } from "http";
+
 const buildArray = (
   values: string[][],
   baseX: number,
@@ -90,19 +92,58 @@ const buildBinaryTree = (
   indexes: string[];
 } => {
   const cells: string[] = [];
+  const indexes: string[] = [];
+
   const levelHeight = sequenceSize + 20;
-  const heapSpacingY = 220; // 各ヒープ木間の縦間隔
+  const heapSpacingY = 220;
   const centerX = 600;
+
+  const getHeapX = (index: number): number => {
+    const level = Math.floor(Math.log2(index + 1));
+    const posInLevel = index - (2 ** level - 1);
+    const x = centerX + (posInLevel - 2 ** (level - 1) / 2) * width * 2;
+    return x;
+  };
+
+  const getHeapPos = (
+    index: number,
+    heapIdx: number
+  ): {
+    x: number;
+    y: number;
+  } => {
+    const level = Math.floor(Math.log2(index + 1));
+    const posInLevel = index - (2 ** level - 1);
+    const x = centerX + (posInLevel - 2 ** (level - 1) / 2) * width * 2;
+    const y = baseX + heapIdx * heapSpacingY + level * levelHeight;
+    return {
+      x: x,
+      y: y,
+    };
+  };
 
   values.forEach((heapLine, heapIdx) => {
     const nodeIds: string[] = [];
 
-    heapLine.forEach((text, index) => {
-      const level = Math.floor(Math.log2(index + 1));
-      const posInLevel = index - (2 ** level - 1);
+    // ヒープラベルの作成 (a), (b), ...
+    const heapLabel = String.fromCharCode(97 + heapIdx); // 97 = 'a'
+    const labelText = `(${heapLabel})`;
+    //heapLine の2次元目の indexが最大の値でlabelXを計算
+    const maxIndex = Math.max(...heapLine.map((line) => line.length));
 
-      const x = centerX + (posInLevel - 2 ** (level - 1) / 2) * width * 2;
-      const y = baseX + heapIdx * heapSpacingY + level * levelHeight;
+    const labelX = centerX - getHeapX(maxIndex) / 2;
+    const labelY = baseX + heapIdx * heapSpacingY - levelHeight / 1.5;
+
+    cells.push(`
+    <mxCell id="${cellId++}"
+      value="${labelText}"
+      style="text;html=1;fontSize=${fontSize};align=center;verticalAlign=middle;strokeColor=none;fillColor=none;"
+      vertex="1" parent="1">
+      <mxGeometry x="${labelX}" y="${labelY}" width="${width}" height="${height}" as="geometry"/>
+    </mxCell>`);
+
+    heapLine.forEach((text, index) => {
+      const { x, y } = getHeapPos(index, heapIdx);
 
       const id = cellId++;
       nodeIds.push(id.toString());
@@ -126,7 +167,7 @@ const buildBinaryTree = (
 
   return {
     cells,
-    indexes: [],
+    indexes,
   };
 };
 
